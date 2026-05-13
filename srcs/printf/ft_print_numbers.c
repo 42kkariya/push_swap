01450751718f57c84e893609cdda5f9baa838b2f
koji18 <karikoji2525@gmail.com>
2026-05-13 15:11:21 +0900

    feat: ft_printf

---
+++ srcs/printf/ft_print_numbers.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkariya <kkariya@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 18:05:02 by kkariya           #+#    #+#             */
+/*   Updated: 2026/05/04 12:00:00 by kkariya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_nbr(int n)
+{
+	long	nb;
+	int		ret;
+
+	nb = n;
+	if (nb < 0)
+	{
+		if (write(1, "-", 1) == -1)
+			return (-1);
+		ret = ft_print_nbr_base((unsigned long)(-nb), "0123456789");
+		if (ret == -1)
+			return (-1);
+		return (1 + ret);
+	}
+	return (ft_print_nbr_base((unsigned long)nb, "0123456789"));
+}
+
+int	ft_print_unsigned(unsigned int n)
+{
+	return (ft_print_nbr_base((unsigned long)n, "0123456789"));
+}
+
+int	ft_print_hex(unsigned int n, char spec)
+{
+	if (spec == 'x')
+		return (ft_print_nbr_base((unsigned long)n, "0123456789abcdef"));
+	return (ft_print_nbr_base((unsigned long)n, "0123456789ABCDEF"));
+}
+
+int	ft_print_ptr(void *ptr)
+{
+	int	ret;
+
+	if (ptr == NULL)
+	{
+		if (write(1, "(nil)", 5) == -1)
+			return (-1);
+		return (5);
+	}
+	if (write(1, "0x", 2) == -1)
+		return (-1);
+	ret = ft_print_nbr_base((unsigned long)ptr, "0123456789abcdef");
+	if (ret == -1)
+		return (-1);
+	return (2 + ret);
+}
---
+++ srcs/printf/includes/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkariya <kkariya@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/29 13:43:55 by kkariya           #+#    #+#             */
+/*   Updated: 2026/04/29 16:04:45 by kkariya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_print_char(int c);
+int	ft_print_str(char *s);
+int	ft_print_nbr(int n);
+int	ft_print_unsigned(unsigned int n);
+int	ft_print_hex(unsigned int n, char spec);
+int	ft_print_ptr(void *ptr);
+int	ft_print_nbr_base(unsigned long n, char *base);
+
+#endif
