5c154d65890f13faff7007068496087c31b90ef8
Matsushima Taisei <mtaisei@c3r2s1.42tokyo.jp>
2026-05-14 17:34:19 +0900

    入力値処理の大枠

---
+++ src/ft_argv_check.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_argv_check.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtaisei <mtaisei@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 16:02:36 by mtaisei           #+#    #+#             */
+/*   Updated: 2026/05/14 17:22:50 by mtaisei          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_argc_check(int argc, char **argv)
+{
+	size_t	i;
+	int		*map;
+	int		nbr;
+
+	map = malloc(sizeof(int) * (argc - 2));
+	if (!map)
+		ft_exit(ERROR);
+	i = 0;
+	while (i < argc)
+	{
+		if ((argv[i][0] >= '0' && argv[i][0] <= '9') || argv[i][0] == '-')
+		{
+			map[i] = ft_atoi(argv[i]);
+		}
+		i++;
+	}
+	ft_nbrcheck(map, argc - 2);
+	free(map);
+	return ;
+}
+
+void	ft_nbrcheck(int *map, int size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (ft_check(map[i], &map[i + 1], size - (i + 1)))
+		{
+			free(map);
+			ft_exit(ERROR);
+		}
+		i++;
+	}
+}
+
+int	ft_check(int nb, int *map, int size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (nb == map[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
