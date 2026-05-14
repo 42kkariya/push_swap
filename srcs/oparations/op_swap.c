03ee9e1c2cdf88202258c01d6a6747788b7e6267
koji18 <karikoji2525@gmail.com>
2026-05-14 14:41:56 +0900

    feat: swapの基幹関数を追加

--- srcs/oparations/op_swap.c
+++ srcs/oparations/op_swap.c
@@ -10,3 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/push_swap.h"
+
+static void	swap(t_list **stack)
+{
+	int	tmp;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	tmp = (*stack)->nbr;
+	(*stack)->nbr = (*stack)->next->nbr;
+	(*stack)->next->nbr = tmp;
+}
+
+void	sa(t_list **stack_a)
+{
+	swap(stack_a);
+	write(1, "sa\n", 3);
+}
+
+void	sb(t_list **stack_b)
+{
+	swap(stack_b);
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_list **stack_a, t_list **stack_b)
+{
+	swap(stack_a);
+	swap(stack_b);
+	write(1, "ss\n", 3);
+}
+
