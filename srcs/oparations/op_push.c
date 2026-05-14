c38b0eaaaee8cecbc8f6400cf2ae38677f1a04bb
koji18 <karikoji2525@gmail.com>
2026-05-14 14:56:36 +0900

    feat: push

--- srcs/oparations/op_push.c
+++ srcs/oparations/op_push.c
@@ -10,3 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/push_swap.h"
+
+static void	push(t_list **from, t_list **to)
+{
+	t_list	*node;
+
+	if (!*from)
+		return ;
+	node = *from;
+	*from = (*from)->next;
+	if (*from)
+		(*from)->prev = NULL;
+	node->next = *to;
+	if (*to)
+		(*to)->prev = node;
+	node->prev = NULL;
+	*to = node;
+}
+
+void	pa(t_list **stack_a, t_list **stack_b)
+{
+	push(stack_b, stack_a);
+	write(1, "pa\n", 3);
+}
+
+void	pb(t_list **stack_a, t_list **stack_b)
+{
+	push(stack_a, stack_b);
+	write(1, "pb\n", 3);
+}
