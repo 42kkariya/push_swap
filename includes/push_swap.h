0b07716c0948295d837c94269de1049dc502d833
koji18 <karikoji2525@gmail.com>
2026-05-18 08:25:28 +0000

    fix

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: kkariya <kkariya@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 15:07:25 by mtaisei           #+#    #+#             */
-/*   Updated: 2026/05/18 17:10:35 by kkariya          ###   ########.fr       */
+/*   Updated: 2026/05/18 17:15:10 by kkariya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ void	rrr(t_list **stack_a, t_list **stack_b);
 /* sort */
 void	sort_simple(t_list **stack_a, t_list **stack_b);
 void	sort_radix(t_list **stack_a, t_list **stack_b);
-void	turk_sort(t_list **stack_a, t_list **stack_b);
+void	sort_turk(t_list **stack_a, t_list **stack_b);
 
 /* sort utils */
 int		stack_size(t_list *stack);
