9d73390ba99013c45acee25f179eda55f8de7470
koji18 <karikoji2525@gmail.com>
2026-05-20 23:17:29 +0000

    chore

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -38,20 +38,44 @@ typedef struct s_flag
 	int	check;
 }	t_frag;
 
+typedef struct s_counts
+{
+	int	cnt_sa;
+	int	cnt_sb;
+	int	cnt_ss;
+	int	cnt_pa;
+	int	cnt_pb;
+	int	cnt_ra;
+	int	cnt_rb;
+	int	cnt_rr;
+	int	cnt_rra;
+	int	cnt_rrb;
+	int	cnt_rrr;
+}	t_counts;
+
+typedef struct s_stacks
+{
+	t_list		**a;
+	t_list		**b;
+	t_counts	*cnt;
+}	t_stacks;
+
 // frag
 
-void	ft_check_frag(char	*argv, t_frag *frag, t_list **cy_list);
+void	ft_check_frag(char *argv, t_frag *frag, t_list **cy_list);
 int		ft_strcmp(char *dest, char *src);
 void	ft_frag_nbr_check(t_frag *frag, t_list **cy_list);
 void	zerofrag(t_frag *frag);
+void	zerocount(t_counts *counts);
+void	print_bench(t_frag *frag, t_counts *counts, double disorder);
 
 // list
 
 void	ft_join_list(t_list **cy_list);
 t_list	*ft_listlast(t_list *cy_list);
 t_list	*ft_listnew(int nb, t_list **cy_list);
-void	ft_make_cy_list(t_list	**cy_list, t_frag *frag, int argc, char **argv);
-void	ft_list_add_back(t_list **cy_list, t_list	*new);
+void	ft_make_cy_list(t_list **cy_list, t_frag *frag, int argc, char **argv);
+void	ft_list_add_back(t_list **cy_list, t_list *node);
 
 // src
 
@@ -63,38 +87,39 @@ void	ft_exit(int error, t_list *cy_list);
 
 // printf
 
-int		ft_printf(const char *format, ...);
-int		ft_print_char(int c);
-int		ft_print_str(char *s);
-int		ft_print_nbr(int n);
-int		ft_print_unsigned(unsigned int n);
-int		ft_print_hex(unsigned int n, char spec);
-int		ft_print_ptr(void *ptr);
-int		ft_print_nbr_base(unsigned long n, char *base);
+int		ft_printf(int fd, const char *format, ...);
+int		ft_print_char(int c, int fd);
+int		ft_print_str(char *s, int fd);
+int		ft_print_nbr(int n, int fd);
+int		ft_print_unsigned(unsigned int n, int fd);
+int		ft_print_hex(unsigned int n, char spec, int fd);
+int		ft_print_ptr(void *ptr, int fd);
+int		ft_print_nbr_base(unsigned long n, char *base, int fd);
+int		ft_print_double(double n, int fd);
 
 /* oparation */
-void	sa(t_list **stack_a);
-void	sb(t_list **stack_b);
-void	ss(t_list **stack_a, t_list **stack_b);
-void	pa(t_list **stack_a, t_list **stack_b);
-void	pb(t_list **stack_a, t_list **stack_b);
-void	ra(t_list **stack_a);
-void	rb(t_list **stack_b);
-void	rr(t_list **stack_a, t_list **stack_b);
-void	rra(t_list **stack_a);
-void	rrb(t_list **stack_b);
-void	rrr(t_list **stack_a, t_list **stack_b);
+void	sa(t_list **stack_a, t_counts *counts);
+void	sb(t_list **stack_b, t_counts *counts);
+void	ss(t_list **stack_a, t_list **stack_b, t_counts *counts);
+void	pa(t_list **stack_a, t_list **stack_b, t_counts *counts);
+void	pb(t_list **stack_a, t_list **stack_b, t_counts *counts);
+void	ra(t_list **stack_a, t_counts *counts);
+void	rb(t_list **stack_b, t_counts *counts);
+void	rr(t_list **stack_a, t_list **stack_b, t_counts *counts);
+void	rra(t_list **stack_a, t_counts *counts);
+void	rrb(t_list **stack_b, t_counts *counts);
+void	rrr(t_list **stack_a, t_list **stack_b, t_counts *counts);
 
 /* sort */
-void	sort_simple(t_list **stack_a, t_list **stack_b);
-void	sort_radix(t_list **stack_a, t_list **stack_b);
-void	sort_turk(t_list **stack_a, t_list **stack_b);
-void	sort_dispatch(t_list **stack_a, t_list **stack_b, t_frag *frag);
+void	sort_simple(t_list **stack_a, t_list **stack_b, t_counts *counts);
+void	sort_radix(t_list **stack_a, t_list **stack_b, t_counts *counts);
+void	sort_turk(t_list **stack_a, t_list **stack_b, t_counts *counts);
+void	sort_dispatch(t_list **sa, t_list **sb, t_frag *frag, t_counts *cnt);
 double	calc_disorder(t_list *stack);
 
 /* sort utils */
 int		stack_size(t_list *stack);
 int		get_rank(t_list *head, t_list *node);
-void	do_move(t_list **sa, t_list **sb, t_list *best, t_list *target);
+void	do_move(t_stacks *st, t_list *best, t_list *target);
 
 #endif
--- main.c
+++ main.c
@@ -14,32 +14,19 @@
 
 int	main(int argc, char **argv)
 {
-	t_list	*cy_list;
-	t_list	*stack_b;
-	t_frag	frag;
+	t_list		*cy_list;
+	t_list		*stack_b;
+	t_frag		frag;
+	t_counts	counts;
 
 	cy_list = NULL;
 	zerofrag(&frag);
+	zerocount(&counts);
 	ft_argv_check(argc, argv, &cy_list);
 	ft_make_cy_list(&cy_list, &frag, argc, argv);
 	if (frag.check == 0)
 		frag.ada = 1;
 	stack_b = NULL;
-	sort_dispatch(&cy_list, &stack_b, &frag);
+	sort_dispatch(&cy_list, &stack_b, &frag, &counts);
 	return (0);
 }
-
-// python3 -c "
-// import subprocess, random
-
-// def test(n, t=20):
-// 	ops = []
-// 	for _ in range(t):
-// 		nums = random.sample(range(-10000,10000), n)
-// 		r = subprocess.run(['./push_swap', '--medium']+list(map(str,nums)), capture_output=True, text=True)
-// 		ops.append(len(r.stdout.strip().splitlines()))
-// 	print(f'n={n}: min={min(ops)}, max={max(ops)}, avg={sum(ops)/len(ops):.1f}')
-
-// test(100)
-// test(500)
-// "
